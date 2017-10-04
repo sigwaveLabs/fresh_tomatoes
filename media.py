c3a01a68fa553465375a226b9f40b086618a536f
sigwaveadmin <marcus@sigwave.com>
2017-10-03 20:04:50 -0700

    Clean Up

--- media.py
+++ media.py
@@ -1,11 +1,13 @@
 import webbrowser
 
-class Movie():
-    
-    
-    def __init__(self, movie_title, movie_year, movie_rating, movie_genre, movie_storyline, poster_image, trailer_youtube):
-        """Creates an instance of the movie class.
 
+class Movie():
+
+    def __init__(self, movie_title,
+                 movie_year, movie_rating,
+                 movie_genre, movie_storyline,
+                 poster_image, trailer_youtube):
+        """Creates an instance of the movie class.
         Args:
             movie_title: The title of the movie.
             movie_year: The year the movie was released.
@@ -14,10 +16,9 @@ class Movie():
             movie_storyline: The plot or storyline of the movie.
             poster_image: A link to the movie poster image.
             trailer_youtube: A link to the youtube movie trailer.
-
         Returns:
             A movie object instance.
-        """           
+        """
         self.title = movie_title
         self.year = movie_year
         self.rating = movie_rating
@@ -27,6 +28,6 @@ class Movie():
         self.trailer_youtube_url = trailer_youtube
 
     def show_trailer(self):
-        """Opens youtube trailer for movie"""       
+        """Opens youtube trailer for movie"""
 
         webbrowser.open(self.trailer_youtube_url)
